37595b5f9f801130ddd1fafd9b86484f48b80750
Adrian Chamorro Silva <adrianchamorrosilva@gmail.com>
2025-09-08 20:30:29 +0200

    test: ZoneCard tests added

---
+++ src/entities/ZoneCard/index.test.jsx
@@ -0,0 +1,56 @@
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { describe, it, expect, vi } from "vitest";
+import { ZoneCard } from "./index";
+
+describe("ZoneCard", () => {
+  const zoneMock = {
+    id: "zone-1",
+    name: "Dormitorio",
+    power: true,
+    temperature: 23,
+    desiredTemp: 22,
+  };
+
+  it("displays the name and allows the user to toggle it on", async () => {
+    const onToggle = vi.fn();
+    const { getByText, getByRole } = render(
+      <ZoneCard zone={zoneMock} onToggle={onToggle} />
+    );
+
+    expect(getByText(/Dormitorio/i)).toBeInTheDocument();
+
+    const powerBtn = getByRole("button", { pressed: true });
+    expect(powerBtn).toBeTruthy();
+    expect(powerBtn).toHaveAttribute("aria-pressed", "true");
+
+    await userEvent.click(powerBtn);
+    expect(onToggle).toHaveBeenCalledWith("zone-1");
+  });
+
+  it("displays the name and allows the user to toggle it off", async () => {
+    const onToggle = vi.fn();
+    const { getByText, getByRole } = render(
+      <ZoneCard zone={{ ...zoneMock, power: false }} onToggle={onToggle} />
+    );
+
+    expect(getByText(/Dormitorio/i)).toBeInTheDocument();
+
+    const powerBtn = getByRole("button", { pressed: false });
+    expect(powerBtn).toBeTruthy();
+    expect(powerBtn).toHaveAttribute("aria-pressed", "false");
+
+    await userEvent.click(powerBtn);
+    expect(onToggle).toHaveBeenCalledWith("zone-1");
+  });
+
+  it("has white background when power is OFF", () => {
+    const onToggle = vi.fn();
+    const { container } = render(
+      <ZoneCard zone={{ ...zoneMock, power: false }} onToggle={onToggle} />
+    );
+
+    const zoneCard = container.querySelector(".zone-card");
+    expect(zoneCard).toHaveClass("state-off");
+  });
+});
