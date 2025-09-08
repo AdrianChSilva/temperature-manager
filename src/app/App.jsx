8e16a2649fabcfed8303413b3fa32b652f3c995b
Adrian Chamorro Silva <adrianchamorrosilva@gmail.com>
2025-09-08 16:48:01 +0200

    chore: initial project setup

---
+++ src/app/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>scaffolding</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,38 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: { "@": path.resolve(__dirname, "src") },
+  },
+  base: "./",
+  build: {
+    outDir: path.resolve(__dirname, "mobile/www"),
+    emptyOutDir: true,
+  },
+  server: {
+    host: true,
+  },
+  test: {
+    environment: "jsdom",
+    setupFiles: ["src/shared/test/setupTests.js"],
+    globals: true,
+    include: ["src/**/*.{test,spec}.{js,jsx}"],
+    coverage: {
+      reporter: ["text", "html", "lcov"],
+      reportsDirectory: "coverage",
+      include: ["src/**/*.{js,jsx}"],
+      exclude: [
+        "src/app/main.jsx",
+        "src/app/App.jsx",
+        "**/*.{test,spec}.?(js|jsx)",
+        "node_modules/**",
+      ],
+    },
+  },
+});
