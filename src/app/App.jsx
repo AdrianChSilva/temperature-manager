f580cc2b129dc6b5ba02fc258492e00752978923
Adrian Chamorro Silva <adrianchamorrosilva@gmail.com>
2025-09-13 19:11:03 +0200

    refactor: changed shared folder to follow best practices of FSD

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,10 +1,12 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 import { HomePage } from "@/pages/Home";
-import { ZoneDetailPage } from "@/pages/ZoneDetail";
 import { NotFoundPage } from "@/pages/NotFound";
+import { ZoneDetailPage } from "@/pages/ZoneDetail";
 import { RealtimeButton } from "@/shared/ui/RealtimeButton";
+import { useRealtimeWS } from "@/shared/api/websocketClient/lib/useRealtimeWS";
 
 function App() {
+  const { active, setActive } = useRealtimeWS();
   return (
     <HashRouter>
       <div className="app-container">
@@ -14,7 +16,7 @@ function App() {
             <Route path="/zone/:zoneId" element={<ZoneDetailPage />} />
             <Route path="*" element={<NotFoundPage />} />
           </Routes>
-          <RealtimeButton />
+          <RealtimeButton isActive={active} setActive={setActive} />
         </main>
       </div>
     </HashRouter>
